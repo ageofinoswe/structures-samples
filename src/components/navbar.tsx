b0289e7c74af66601ab14a3f67cb4adc87b4a41b
Alexander Fino <afino@uci.edu>
2025-08-25 19:09:14 -0700

    added naar options

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,32 +1,35 @@
-import { AppBar, Box, Grid, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Grid, Stack, Tab, Tabs, Toolbar, Typography } from "@mui/material"
+import React from "react"
 
 
 function NavBar() {
+    const [value, setValue] = React.useState('primary');
 
-  return (
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+
+    return (
     <>
         <AppBar position="static" color="default" elevation={0} sx={{backgroundColor: 'white'}}>
-            <Toolbar variant='regular' disableGutters={true}>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container>
-                        <Grid>
-                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                <img src='/src/assets/bridge.png' style={{height: 60, width: 'auto'}}></img>
-                                <Typography fontSize={50} fontStyle='italic' fontFamily='cursive'>S</Typography>
-                            </Box>
-                        </Grid>
-                        <Grid sx={{ alignContent: 'center' }}>
-                            <Box>
-                                <Typography pt={0.4} lineHeight={1} fontSize={20} fontWeight='medium' fontStyle='oblique'>tructural</Typography>
-                                <Typography lineHeight={1} fontSize={20} fontWeight='medium' fontStyle='oblique'>amples</Typography>
-                            </Box>
-                        </Grid>
-                    </Grid>
+            <Toolbar disableGutters={true}>
+                <img src='/src/assets/bridge.png' style={{height: 60, width: 'auto'}}></img>
+                <Typography fontSize={50} fontStyle='italic' fontFamily='cursive'>S</Typography>
+                <Stack>
+                    <Typography pt={0.5} lineHeight={1} fontSize={25} fontWeight='medium' fontStyle='oblique'>tructural</Typography>
+                    <Typography lineHeight={1} fontSize={20} fontWeight='medium' fontStyle='oblique'>amples</Typography>
+                </Stack>
+                <Box sx={{ justifyItems: 'center', flexGrow: 1 }}>
+                    <Tabs value={value} onChange={handleChange} textColor="primary" indicatorColor="secondary" aria-label="secondary tabs example">
+                        <Tab value='one' label='Shear & Moment Diagrams'/>
+                        <Tab value='two' label='Foundation Bearing Pressure'/>
+                        <Tab value='three' label='Pier Foundation Depth'/>
+                    </Tabs>
                 </Box>
             </Toolbar>
         </AppBar>
     </>
-  )
+    )
 }
 
 export default NavBar
