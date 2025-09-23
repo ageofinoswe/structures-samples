301fc6a6ea359a5edc54cd5b71a1330dfad64bce
Alexander <ageofinoswe@gmail.com>
2025-09-23 01:22:12 -0700

    removed app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import Navbar from './components/Navbar'
 import Foundation from './pages/Foundation'
 import Pier from './pages/Pier'
